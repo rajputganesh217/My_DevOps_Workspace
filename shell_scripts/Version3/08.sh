d46dd81acac4b8fb0f6db948bf40cb9761ddbdb8
Ganesh charawande <159476491+rajputganesh217@users.noreply.github.com>
2025-10-28 19:10:39 +0530

    Update script version and date in 08.sh

--- shell_scripts/Version3/08.sh
+++ shell_scripts/Version3/08.sh
@@ -1,7 +1,7 @@
 ###############################################################################
 #     Script Name   :  check_site_status.sh                                 #               
-#     Version       :  2.0                                                  #                 
-#     Date          :  17 Oct 2025                                          #               
+#     Version       :  3.0                                                  #                 
+#     Date          :  28 Oct 2025                                          #               
 #     Credit        :  Team CloudEthix                                      #             
 #     Purpose       :  Shell Script for checking the staus of site using    #
 #                           curl.                                           #
