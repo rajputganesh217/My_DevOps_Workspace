806eeee0a3b398b90a793c3590d6199bd08c76a6
Ganesh charawande <159476491+rajputganesh217@users.noreply.github.com>
2025-10-28 19:21:50 +0530

    Update script version and improve error handlingup

--- shell_scripts/Version3/01.sh
+++ shell_scripts/Version3/01.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-
-
 ############################################################################
 #     Script Name   :  list_directory_files.sh                             #               
-#     Version       :  2.0                                                 #                 
-#     Date          :  17 Oct 2025                                         #               
+#     Version       :  3.0                                                 #                 
+#     Date          :  28 Oct 2025                                         #               
 #     Credit        :  Team CloudEthix                                     #             
-#     Purpose       :  A shell script program file to list given directory # 
-#                     files and store result in directory_content.txt fie. #
+#     Purpose       :  Lists files in a given directory (passed as argument)#
+#                     and saves output to directory_content.txt file.      #
 ###########################################################################
 
+DIR=$1  
 
-read -p "ENTER THE DIRECTORY  NAME WITH PATH YOU WANT TO LIST :- " DIR
 
-if [[ ! -d "$DIR" ]]; then
-    echo "NOT A VALID DIRECTORY PLEASE CHECK NAME AND PATH YOU GIVEN ."
-else
-    {
-        echo "------------------------- $(date) -------------------------"
-        echo "Directory: $DIR"
-        echo "------------------------------------------------------------"
-        ls "$DIR"
-        echo ""
-    } >> directory_content.txt
-    echo " list of files added to  directory_content.txt"
+if [[ -z "$DIR" ]]; then
+    echo "Usage: $0 <directory_path>"
+    exit 1
 fi
 
+
+if [[ ! -d "$DIR" ]]; then
+    echo "Error: '$DIR' is not a valid directory."
+    exit 1
+fi
+
+
+{
+    echo "------------------------- $(date) -------------------------"
+    echo "Directory: $DIR"
+    echo "------------------------------------------------------------"
+    ls "$DIR"
+    echo ""
+} >> directory_content.txt
+
+echo "List of files added to directory_content.txt"
