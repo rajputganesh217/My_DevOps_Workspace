c1d3d9ba5ab89dc9fad780f3bf7a2fd1dd661e23
Ganesh charawande <159476491+rajputganesh217@users.noreply.github.com>
2025-10-29 10:16:10 +0530

    Add script to extract IP details from JSON file
    This script reads a JSON file containing AWS EC2 instance details and extracts the instance ID along with its private and public IP addresses.

---
+++ shell_scripts/Version2/10.sh
@@ -0,0 +1,17 @@
+###############################################################################
+#     Script Name   :  10_ip_details.sh                                
+#     Version       :  2.0                                                   
+#     Date          :  22 Oct 2025                                           
+#     Credit        :  Team CloudEthix                                       
+#     Purpose       : find ip adresses of instances           
+###############################################################################
+
+
+#!/bin/bash
+
+read -p "Enter the JSON file name : " JSON_FILE
+#aws ec2 describe-instances \
+ # --query 'Reservations[].Instances[].{ID:InstanceId,Private:PrivateIpAddress,Public:PublicIpAddress}' \
+ # --output json > instances.json
+
+ jq -r '.Reservations[].Instances[] | "Instance ID: \(.InstanceId), Private IP: \(.PrivateIpAddress), Public IP: \(.PublicIpAddress)"' "$JSON_FILE"
