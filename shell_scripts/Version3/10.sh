88bde995a526f9c884a03477ce5108b415ee87ca
Ganesh charawande <159476491+rajputganesh217@users.noreply.github.com>
2025-10-29 10:05:52 +0530

    Add metadata and purpose to instance_ips.sh
    Added script header with metadata and purpose.

--- shell_scripts/Version3/10.sh
+++ shell_scripts/Version3/10.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+###############################################################################
+#     Script Name   :  instance_ips.sh                                
+#     Version       :  3.0                                                   
+#     Date          :  28 Oct 2025                                           
+#     Credit        :  Team CloudEthix                                       
+#     Purpose       : find ip adresses of instances           
+###############################################################################
+
+
+
+#!/bin/bash
 
 read -p "Enter the JSON file name : " JSON_FILE
 #aws ec2 describe-instances \
